96c96e23110f1f4a3bb3ed348ff6c2ef36363479
blairholmgren <blair.holmgren@gmail.com>
2019-05-07 12:01:57 -0700

    Started reading list project

---
+++ ReadingList/ReadingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadingList
+//
+//  Created by Blair Holmgren on 5/7/19.
+//  Copyright © 2019 Blair Holmgren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
